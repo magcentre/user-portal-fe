54d63ed0bffb20ee87b38f5655ad9c29180989bf
Prasanna Mangoli <Prasanna Mangoli>
2022-02-26 20:47:52 +0530

    help-center init

--- src/utils/consumtion.js
+++ src/utils/consumtion.js
@@ -2,13 +2,11 @@ import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
-import { Typography } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 const ConsumtionCard = styled(Paper)(({ theme }) => ({
-  ...theme.typography.body2,
-  padding: theme.spacing(2),
+  padding: theme.spacing(1),
   paddingLeft: theme.spacing(2),
   alignContent: 'center',
   color: theme.palette.secondary.dark,
@@ -23,16 +21,14 @@ const StorageConsumption = (props) => {
     <>
       <ConsumtionCard>
         <Grid container
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center">
+           direction="row"
+           justifyContent="space-between"
+           alignItems="center">
           <Grid item>
-            <Typography variant="h5" gutterBottom component="div">
-              Storage Consumption
-            </Typography>
+            Storage Consumption    
           </Grid>
           <Grid item >
-            <IconButton aria-label="delete" size="small" color="primary">
+            <IconButton aria-label="storage-consumtion" size="small" color="primary">
               <ArrowForwardIosIcon fontSize="small" />
             </IconButton>
           </Grid>
